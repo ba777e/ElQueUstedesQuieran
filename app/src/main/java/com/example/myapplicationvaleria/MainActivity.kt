ffcb51c4197e68a6b5b50024fa8c3ceb21ed10ec
Valeria Mejía Osornio <vamejosor@gmail.com>
2026-03-04 21:21:15 -0600

    Clase 4 de marzo 2026

--- app/src/main/java/com/example/myapplicationvaleria/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationvaleria/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplicationvaleria //nombre del paquete
 //Package define dónde vive mi código. Usa una DNS inverso para mantener el nombre de mi paquete único en la google play
 
 import android.os.Bundle //dependencias de nuestra actividad
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() { //indica el nombre de la clase
     override fun onCreate(savedInstanceState: Bundle?) { //los dos puntos que indican la herencia de la funcionalidad de appcompatactivity
         super.onCreate(savedInstanceState)
+        Log.i("myapp", "HOLA VALERIA")
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -21,10 +24,13 @@ class MainActivity : AppCompatActivity() { //indica el nombre de la clase
 
     override fun onStart() { //override indica que estamos proporcionando una implementación
         super.onStart()
+        Log.i("myapp", "HOLA VALERIA")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.i("myapp", "HOLA VALERIA")
+
     }
 }
 
